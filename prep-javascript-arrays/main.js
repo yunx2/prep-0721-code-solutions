66a3666fe75ec914f0ec6321747ea0e713b6f670
Yunyun Liu <celeph4is@gmail.com>
2021-08-07 16:41:49 -0700

    javascript arrays exercise

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+
+const firstElement = languages[0];
+console.log('the first element of the array is', firstElement);
+
+const thirdElement = languages[2];
+console.log('the third element of the array is', thirdElement);
+
+const length = languages.length;
+console.log('the length of the array is', length);
+
+const lastElement = languages[length - 1];
+console.log('the last element of the array is', lastElement);
