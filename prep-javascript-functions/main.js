5f10455cb09596e4db7e81dac8eca066b02cfcb6
Yunyun Liu <celeph4is@gmail.com>
2021-08-11 12:15:01 -0700

    add some function expressions; finish exercise

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -1,10 +1,12 @@
+// 1. addTwoNumbers
 function addTwoNumbers(x, y) {
   return x + y;
 }
 
-var sum = addTwoNumbers(4, 4);
-console.log('sum', sum);
+const sum = addTwoNumbers(4, 4);
+console.log('expected:', 8, 'actual:', sum);
 
+// 2. convertHoursToMinutes
 function convertHoursToMinutes(hours) {
   return hours * 60;
 }
@@ -12,29 +14,31 @@ function convertHoursToMinutes(hours) {
 const minutes = convertHoursToMinutes(2);
 console.log('expect: 120', 'actual:', minutes);
 
+// 3. getGreeting
 function getGreeting(name) {
   return `Hello ${name}!`;
 }
 
 const greeting = getGreeting('Yunyun');
-
 console.log('expect:', 'Hello Yunyun!', 'actual:', greeting);
 
-function addAndMultiplyBy5(num1, num2) {
+// 4. addAndMultiplyBy5
+const addAndMultiplyBy5 = function (num1, num2) {
   return (num1 + num2) * 5;
-}
+};
 
 const addAndMultiplyResult = addAndMultiplyBy5(10, 5);
-
 console.log('expect:', 15 * 5, 'actual:', addAndMultiplyResult);
 
-function multiplyAndDivideBy5(num1, num2) {
+// 5. multiplyAndDivideBy5
+const multiplyAndDivideBy5 = function (num1, num2) {
   return (num1 * num2) / 5;
-}
+};
 
 const multiplyAndDivideResult = multiplyAndDivideBy5(35, 10);
 console.log(`expect: ${(35 * 10) / 5} actual: ${multiplyAndDivideResult}`);
 
+// 6. subtractTwoNumbers
 function subtractTwoNumbers(num1, num2) {
   return num1 - num2;
 }
@@ -42,6 +46,7 @@ function subtractTwoNumbers(num1, num2) {
 const difference = subtractTwoNumbers(22, 7);
 console.log('expect:', 22 - 7, 'actual:', difference);
 
+// 7. getCircleCircumference
 function getCircleCircumference(radius) {
   return 2 * Math.PI * radius;
 }
@@ -49,13 +54,15 @@ function getCircleCircumference(radius) {
 const circ = getCircleCircumference(5);
 console.log('expect:', 2 * Math.PI * 5, 'actual:', circ);
 
-function getFullName(firstName, lastName) {
+// 8. getFullName
+const getFullName = function (firstName, lastName) {
   return firstName + ' ' + lastName;
-}
+};
 
 const fullNameResult = getFullName('Valerie', 'Liu');
 console.log(`expect: Valerie Liu actual: ${fullNameResult}`);
 
+// 9. cube
 function cube(number) {
   return number * number * number;
 }
