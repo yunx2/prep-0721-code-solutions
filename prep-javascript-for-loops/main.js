0b4ff89e40e1ad8b6a5c94604a76671be5882d02
Yunyun Liu <celeph4is@gmail.com>
2021-08-11 12:40:19 -0700

    complete for-loop exercise

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,17 +1,16 @@
 // create your loops here
 
-// loop runs 10 times, incrememnts by 1
+console.log('loop runs 10 times, increments by 1; prints 0 to 9');
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
-// loop runs 10 times and increments by 2
+console.log('loop runs 10 times, increments by 2');
 for (let i = 0; i < 20; i += 2) {
   console.log(i);
 }
 
-// loop runs 100 times; i decrements by 1 each time
-
+console.log('loop runs 100 times; i decrements by 1 each time');
 for (let i = 100; i > 0; i--) {
   console.log('Time til explosion: ' + i + '!');
 }
